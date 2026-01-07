6688d0b2f502c48a71a1e8112eb5a49e5601e592
Navid Talebian <navidt81@gmail.com>
2026-01-07 22:37:07 +0330

    chore: update tdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-	entry: 'src/index.ts',
-	format: ['cjs', 'esm'],
+	entry: {
+		index: './src/index.ts',
+		'zk/index': './src/zod/index.ts',
+		'vk/index': './src/valibot/index.ts',
+	},
+	format: ['esm'],
 	dts: true,
 	clean: true,
 	sourcemap: true,
 	platform: 'node',
 	target: 'es2020',
 	outDir: 'dist',
+	publint: true,
 });
