620fab2df7955721a4cf38c1fabc03827c6d81ce
Navid Talebian <navidt81@gmail.com>
2026-02-23 00:52:15 +0330

    feat: add some regexes

--- src/core/shared/regexes.ts
+++ src/core/shared/regexes.ts
@@ -1,1 +1,7 @@
 export const symbolRegex = '[!@#$%^&*(),.?":{}|<>]';
+export const nicknameRegex = /^[a-zA-Z0-9_ .-]+$/;
+export const usernameRegex = /^[a-z0-9_]+$/;
+export const noDigitRegex = /^\D*$/;
+export const noConsecutiveSpecialRegex = /^(?!.*([_.-])\1)/;
+export const unicodeRegex = /^[\p{L}\p{M}\p{N}_ .-]+$/u;
+export const noSpacesRegex = /^\S$/;
